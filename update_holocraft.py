f3902bff36e06fea3815652bbb55eab993374682
Jeff Tao <jatao88@gmail.com>
2021-02-08 00:01:38 -0500

    Support source streams + initial data fetch

--- update_holocraft.py
+++ update_holocraft.py
@@ -1,20 +1,38 @@
+import json
 import sys
+import re
 import requests
 import googleapiclient.discovery as api  # type: ignore
 from itertools import chain
-from typing import Dict, List
+from typing import Dict, List, Set
+from timeit import timeit
 from dataclasses import dataclass, field
-from dataclasses_json import DataClassJsonMixin
+from dataclasses_json import DataClassJsonMixin, config
 from datetime import datetime
+from marshmallow import fields
+from dateutil.parser import isoparse
 
 DATAFILE_PATH = "docs/data/holocraft.json"
 
 
 @dataclass
 class HolocraftStream(DataClassJsonMixin):
-    streamed_on: datetime
-    video_id: str
-    clips: List[str]
+    # Member Channel ID whose source stream this is
+    member: str
+    # When the stream was published
+    published_at: datetime = field(
+        metadata=config(
+            encoder=datetime.isoformat,
+            decoder=datetime.fromisoformat,
+            mm_field=fields.DateTime(format="iso"),
+        )
+    )
+
+
+@dataclass
+class HolocraftClip(DataClassJsonMixin):
+    # Video IDs of source streams
+    source_streams: List[str]
 
 
 @dataclass
@@ -26,7 +44,7 @@ class HolocraftData(DataClassJsonMixin):
     # Channel ID -> Upload Playlist ID
     upload_playlists: Dict[str, str] = field(default_factory=dict)
     # Channel ID -> Video ID
-    seen_videos: Dict[str, str] = field(default_factory=dict)
+    seen_videos: Dict[str, Set[str]] = field(default_factory=dict)
     # Video ID -> HolocraftStream
     craft_streams: Dict[str, HolocraftStream] = field(default_factory=dict)
     # Video ID -> [Source Stream IDs]
@@ -50,34 +68,112 @@ def ensure_upload_playlists(youtube: api.Resource, data: HolocraftData):
         if not channel_id in data.upload_playlists:
             upload_playlist_id = get_upload_playlist_id(youtube, channel_id)
             if not upload_playlist_id is None:
+                print(
+                    f"Now tracking upload playlist {upload_playlist_id} for channel {channel_id}"
+                )
                 data.upload_playlists[channel_id] = upload_playlist_id
+            else:
+                print(f"Failed to get upload playlist for {channel_id}")
+
+
+def playlist_videos(youtube: api.Resource, playlist_id: str):
+    page_token = None
+    while True:
+        request = youtube.playlistItems().list(
+            part="contentDetails,id,snippet",
+            maxResults=50,
+            playlistId=playlist_id,
+            pageToken=page_token,
+        )
+        response = request.execute()
+
+        for playlist_item in response["items"]:
+            yield playlist_item
+
+        if not "nextPageToken" in response:
+            break
+        page_token = response["nextPageToken"]
+
+
+def is_minecraft_video(playlist_item):
+    # If we see minecraft in the video title we can save some time
+    normalized_title = playlist_item["snippet"]["title"].lower()
+    if any(indicator in normalized_title for indicator in ["minecraft", "マイクラ"]):
+        return True
+
+    # Otherwise we try the weird strategy of finding the explicit string "Minecraft" with quotes
+    # which matches the a javascript payload on the video page for the meta game info block.
+    # This will probably break some day in the distant future.
+    #
+    # Why would YouTube add this useful metadata to the API? Don't be ridiculous.
+    video_page = requests.get(
+        f"https://youtube.com/watch?v={playlist_item['contentDetails']['videoId']}"
+    )
+    return re.search(r"\"simpleText\":\"Minecraft\"", video_page.text) is not None
 
 
 def update_source_streams(youtube: api.Resource, data: HolocraftData):
-    pass
+    for member_name, member_channel_id in data.members.items():
+        print("Processing:", member_name)
+        upload_playlist_id = data.upload_playlists[member_channel_id]
+        if not member_channel_id in data.seen_videos:
+            data.seen_videos[member_channel_id] = set()
+
+        for playlist_item in playlist_videos(youtube, upload_playlist_id):
+            snippet = playlist_item["snippet"]
+            content_details = playlist_item["contentDetails"]
+
+            video_id = content_details["videoId"]
+            if not video_id in data.seen_videos[member_channel_id]:
+                if is_minecraft_video(playlist_item):
+                    print(f"{video_id}: {snippet['channelTitle']} - {snippet['title']}")
+                    published_at = isoparse(content_details["videoPublishedAt"])
+                    # Add this source stream to the holocraft database
+                    data.craft_streams[video_id] = HolocraftStream(
+                        member_channel_id, published_at
+                    )
+
+                # Mark this video as seen so we don't process it again
+                data.seen_videos[member_channel_id].add(video_id)
+
+        # Checkpoint the data to disk after each channel
+        write_data(data)
 
 
 def update_clips(youtube: api.Resource, data: HolocraftData):
     pass
 
 
+def load_data():
+    with open(DATAFILE_PATH, "r") as holocraft_data_file:
+        data = HolocraftData.from_json(holocraft_data_file.read())
+        return data
+
+
+def do_write_data(data: HolocraftData):
+    with open(DATAFILE_PATH, "w") as holocraft_data_file:
+        holocraft_data_file.write(json.dumps(data.to_dict(), indent=2))
+
+
+def write_data(data: HolocraftData):
+    write_time = timeit(lambda: do_write_data(data), setup="gc.enable()", number=1)
+    print(f"Wrote data in {write_time} seconds:")
+
+
 def main():
     api_key = sys.argv[1]
     youtube = api.build("youtube", "v3", developerKey=api_key)
 
-    with open(DATAFILE_PATH, "r+") as holocraft_data_file:
-        # Load existing data
-        data = HolocraftData.from_json(holocraft_data_file.read())
+    # Load existing data
+    data = load_data()
 
-        # Do update
-        ensure_upload_playlists(youtube, data)
-        update_source_streams(youtube, data)
-        update_clips(youtube, data)
+    # Do update
+    ensure_upload_playlists(youtube, data)
+    update_source_streams(youtube, data)
+    update_clips(youtube, data)
 
-        # Write out
-        holocraft_data_file.seek(0)
-        holocraft_data_file.write(data.to_json())
-        holocraft_data_file.truncate()
+    # Write out
+    write_data(data)
 
 
 if __name__ == "__main__":
